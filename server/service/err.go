54dc845cefc83b2de8ea918c8af5d0c263382993
ja88a <r0g3r@tuta.io>
2023-11-17 12:23:29 +0100

    tidy module deps

--- server/service/err.go
+++ server/service/err.go
@@ -7,19 +7,19 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (g *VerifiableRemoteFileStorageServer) logError(err error) error {
+func (g *VerifiableRemoteFileStorageServer) LogError(err error) error {
 	if err != nil {
 		g.l.Debug(err)
 	}
 	return err
 }
 
-func (g *VerifiableRemoteFileStorageServer) contextError(ctx context.Context) error {
+func (g *VerifiableRemoteFileStorageServer) ContextError(ctx context.Context) error {
 	switch ctx.Err() {
 	case context.Canceled:
-		return g.logError(status.Error(codes.Canceled, "request is canceled"))
+		return g.LogError(status.Error(codes.Canceled, "request is canceled"))
 	case context.DeadlineExceeded:
-		return g.logError(status.Error(codes.DeadlineExceeded, "deadline is exceeded"))
+		return g.LogError(status.Error(codes.DeadlineExceeded, "deadline is exceeded"))
 	default:
 		return nil
 	}
