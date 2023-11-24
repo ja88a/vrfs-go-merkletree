22028c8192e700090a765c5938bd6748973409c3
ja88a <r0g3r@tuta.io>
2023-11-24 17:18:44 +0100

    support for generating or not the tree proofs when retrieving the common/default MT config

--- libs/merkletree/config.go
+++ libs/merkletree/config.go
@@ -2,11 +2,16 @@ package merkletree
 
 import "github.com/ja88a/vrfs-go-merkletree/libs/merkletree/hash"
 
-func MerkleTreeDefaultConfig() *Config {
+// Commonly shared default config for generating Merkle Trees from filesets' file hashes
+func MerkleTreeDefaultConfig(proofsGen bool) *Config {
+	treeGenMode := ModeTreeBuild
+	if proofsGen {
+		treeGenMode = ModeProofGenAndTreeBuild
+	}
 	return &Config{
 		HashFunc:           hash.DefaultHashFuncParallel,
 		NumRoutines:        0,
-		Mode:               ModeTreeBuild,
+		Mode:               treeGenMode,
 		RunInParallel:      true,
 		SortSiblingPairs:   false,
 		DisableLeafHashing: true,
