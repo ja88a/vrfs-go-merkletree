796e037118c7d7034d2a4ebd86ab645fd2f14a45
ja88a <r0g3r@tuta.io>
2023-11-24 18:07:38 +0100

    finalize the downloaded files' MT-based verification

--- client/app/downloader.go
+++ client/app/downloader.go
@@ -1,14 +1,15 @@
 package app
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
-	// "strings"
+	"strings"
 
 	srvctx "github.com/ja88a/vrfs-go-merkletree/client/rservice"
-	// mt "github.com/ja88a/vrfs-go-merkletree/libs/merkletree"
-	mtfiles "github.com/ja88a/vrfs-go-merkletree/libs/merkletree/files"
+	mt "github.com/ja88a/vrfs-go-merkletree/libs/merkletree"
+	mtutils "github.com/ja88a/vrfs-go-merkletree/libs/merkletree/utils"
 )
 
 // Batch upload of local files to the Remote FS store
@@ -16,14 +17,13 @@ func DownloadFile(ctx *srvctx.ApiService, fileSetId string, fileIndex int) error
 	log.Printf("Downloading file #%v part of fileset '%v'", fileIndex, fileSetId)
 
 	// 1. Retrieve necessary download & verification proofs from VRFS
-	bucketId, mtProofs, err := ctx.HandleDownloadFileInfoReq(srvctx.UserMock, fileSetId, fileIndex)
+	bucketId, mtProof, err := ctx.HandleDownloadFileInfoReq(srvctx.UserMock, fileSetId, fileIndex)
 	if err != nil {
 		return fmt.Errorf("failed at retrieving file download info from VRFS for file %d in fileset '%v'\n%w", fileIndex, fileSetId, err)
 	}
-	if mtProofs == nil {
-		// FIXME Trigger an actual error & end the download process as long as file is not verifiable
-		//return fmt.Errorf("missing the merkle proofs from VFS to check the consistency of file '%d' in fileset '%v'", fileIndex, fileSetId)
-		log.Printf("ERROR missing the merkle proofs from VFS to check the consistency of file '%d' in fileset '%v'", fileIndex, fileSetId)
+	if mtProof == nil {
+		// Trigger an actual error & end the download process as long as file is not verifiable
+		return fmt.Errorf("missing the merkle tree proofs from VRFS to check for the consistency of file %4d in fileset '%v'", fileIndex, fileSetId)
 	}
 
 	// 2. Save the file locally, in the client download dir
@@ -52,18 +52,30 @@ func DownloadFile(ctx *srvctx.ApiService, fileSetId string, fileIndex int) error
 
 	// 3. Verify the downloaded file's hash based on the fileset's MerkleTree root 
 	// and the MerkleTree proofs retrieved from VRFS
-	fileHashes, err := mtfiles.ComputeFileHashes([]string{localFilePath})
+	fileHashes, err := mtutils.ComputeFileHashes([]string{localFilePath})
 	if err != nil {
 		return fmt.Errorf("failed to compute hash for file '%v' \n%w", localFilePath, err)
 	}
-	log.Printf("File '%v' downloaded Hash: %v", localFilePath, fileHashes[0])
+	log.Printf("File '%v' Downloaded. Hash: %v", localFilePath, fileHashes[0])
 
-	// rootHash := strings.TrimPrefix("fs-", fileSetId)
-	// fileBlock := &mt.DataBlock{
-	// 	Data: []byte(fileHashes[0]),
-	// }
-	// mtDefaultConfig := mt.MerkleTreeDefaultConfig()
-	// ok, err := mt.Verify(fileBlock, mtProofs, []byte(rootHash), mtDefaultConfig.HashFunc)
+	rootHashS := strings.TrimPrefix(fileSetId, FILESET_PREFIX) // Trick for avoiding the local storage of the fileset's MT root
+	rootHash, err := hex.DecodeString(rootHashS)
+	if err != nil {
+		return fmt.Errorf("failed to convert root hash to hex '%v'", rootHashS)
+	}
+
+	fileBlock := &mt.DataBlock{
+		Data: []byte(fileHashes[0]), // TODO No string conversion but hex bytes - Remove unecessary string conversions
+	}
+	mtConfig := mt.MerkleTreeDefaultConfig(false)
+	fileValid, err := mt.Verify(fileBlock, mtProof, rootHash, mtConfig)
+	if err != nil {
+		return fmt.Errorf("failed to verify the downloaded file '%v' \n%w", localFilePath, err)
+	}
+	if !fileValid {
+		return fmt.Errorf("downloaded file '%v' has failed the verification process - Root: '%v' ProofSiblings: %d Hash: '%v'", localFilePath, rootHashS, len(mtProof.Siblings), fileHashes[0])
+	} 
+	log.Printf("Downloaded file '%v' is successfully verified as untampered!", localFilePath)
 
 	return nil
 }
