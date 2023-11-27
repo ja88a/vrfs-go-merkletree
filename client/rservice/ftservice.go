97a281041830fdde2ccfc2b0192a456117c6a635
ja88a <r0g3r@tuta.io>
2023-11-27 14:26:00 +0100

    align the file upload & download methods

--- client/rservice/ftservice.go
+++ client/rservice/ftservice.go
@@ -14,8 +14,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	pb "github.com/ja88a/vrfs-go-merkletree/libs/rpcapi/protos/v1/fileserver"
 	rpcfile "github.com/ja88a/vrfs-go-merkletree/libs/rpcapi/file"
+	pb "github.com/ja88a/vrfs-go-merkletree/libs/rpcapi/protos/v1/fileserver"
 )
 
 // File transfer service context info
@@ -23,15 +23,15 @@ type FTService struct {
 	addr      string
 	batchSize int
 	client    pb.FileServiceClient
-	debug			bool
+	debug     bool
 }
 
 // Init the file transfer service context info
-func NewFileTransfer(addr string, batchSize int) *FTService {
+func NewFileTransfer(addr string, batchSize int, verbose bool) *FTService {
 	return &FTService{
 		addr:      addr,
 		batchSize: batchSize,
-		debug: 		 false,
+		debug:     verbose,
 	}
 }
 
@@ -75,25 +75,30 @@ func (s *FTService) upload(ctx context.Context, cancel context.CancelFunc, bucke
 	if err != nil {
 		return err
 	}
-	// if s.debug {}
-	log.Printf("Sent %6d bytes for file %s\n", res.GetSize(), res.GetFileName())
-
+	if s.debug {
+		log.Printf("Sent %6d bytes for file %s\n", res.GetSize(), res.GetFileName())
+	}
 	cancel()
 	return nil
+}
+
+func (s *FTService) initClientConnection() (*grpc.ClientConn, error) {
+	conn, err := grpc.Dial(s.addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to FS grpc API at '%v'\n%v", s.addr, err)
+	}
+	
+	s.client = pb.NewFileServiceClient(conn)
+
+	return conn, nil
 }
 
 // Local file data transfer protocol based on gRPC streaming, in chunks
 func (s *FTService) SendFile(bucketId string, filePath string) error {
 	if s.debug {
-		log.Printf("Sending file '%v' to '%v'", filePath, s.addr)
+		log.Printf("Sending file '%v' to FS bucket '%v' at '%v'", filePath, bucketId, s.addr)
 	}
-	conn, err := grpc.Dial(s.addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
 
-	s.client = pb.NewFileServiceClient(conn)
 	interrupt := make(chan os.Signal, 1)
 	shutdownSignals := []os.Signal{
 		os.Interrupt,
@@ -103,12 +108,21 @@ func (s *FTService) SendFile(bucketId string, filePath string) error {
 	}
 
 	signal.Notify(interrupt, shutdownSignals...)
+
+	conn, err := s.initClientConnection()
+	if err != nil {
+		return fmt.Errorf("failed to init upload of file '%v' to the FileStorage service \n%w", filePath, err)
+	}
+	defer conn.Close()
+
+	s.client = pb.NewFileServiceClient(conn)
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	go func(s *FTService) {
-		if err = s.upload(ctx, cancel, bucketId, filePath); err != nil {
-			log.Fatal(err)
+		if err := s.upload(ctx, cancel, bucketId, filePath); err != nil {
+			log.Fatalf("Upload of file '%v' to bucket '%v' has failed\n%v", filePath, bucketId, err)
 			cancel()
 		}
 	}(s)
@@ -126,56 +140,55 @@ func (s *FTService) SendFile(bucketId string, filePath string) error {
 // Consider the file index towards a lexically sorted list order of the target files directory
 func (s *FTService) DownloadFile(bucketId string, fileIndex int) (*rpcfile.File, error) {
 	if s.debug {
-		log.Printf("Download file #%d from bucket '%v'", fileIndex, bucketId)
+		log.Printf("Downloading file #%d from FS bucket '%v' at '%v'", fileIndex, bucketId, s.addr)
 	}
-	conn, err := grpc.Dial(s.addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := s.initClientConnection()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to init the FileStorage download service \n%w", err)
 	}
 	defer conn.Close()
 
-	s.client = pb.NewFileServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(2)*time.Second)
 	defer cancel()
-	
-	resp, err := s.client.Download(ctx, &pb.FileDownloadRequest{BucketId: bucketId, FileIndex: int32(fileIndex)})
-	if (err != nil) {
-		return nil, fmt.Errorf("failed to download file #%d from bucket '%v'\n%w", fileIndex, bucketId, err)
-	}
-	
-	md, err := resp.Header()
+
+	stream, err := s.client.Download(ctx, &pb.FileDownloadRequest{BucketId: bucketId, FileIndex: int32(fileIndex)})
 	if err != nil {
-		return nil, fmt.Errorf("invalid file download response header for file #%d from bucket '%v'\n%w", fileIndex, bucketId, err)
+		return nil, fmt.Errorf("failed to download file #%d from FS bucket '%v' \n%w", fileIndex, bucketId, err)
+	}
+
+	md, err := stream.Header()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve download stream header for file #%d from bucket '%v'\n%w", fileIndex, bucketId, err)
 	}
 
 	pReader, pWriter := io.Pipe()
 	rFile := rpcfile.NewFromMetadata(md, pReader)
-	go copyFromResponse(pWriter, resp)
+	go copyFromResponse(pWriter, stream)
 
 	return rFile, nil
 }
 
-func copyFromResponse(w *io.PipeWriter, res pb.FileService_DownloadClient) {
+func copyFromResponse(w *io.PipeWriter, stream pb.FileService_DownloadClient) {
 	message := new(pb.FileDownloadResponse)
 	var err error
 	for {
-		 err = res.RecvMsg(message)
-		 if err == io.EOF {
-				_ = w.Close()
+		err = stream.RecvMsg(message)
+		if err == io.EOF {
+			_ = w.Close()
+			break
+		}
+		if err != nil {
+			_ = w.CloseWithError(err)
+			break
+		}
+		if len(message.GetChunk()) > 0 {
+			_, err = w.Write(message.Chunk)
+			if err != nil {
+				_ = stream.CloseSend()
 				break
-		 }
-		 if err != nil {
-				_ = w.CloseWithError(err)
-				break
-		 }
-		 if len(message.GetChunk()) > 0 {
-				_, err = w.Write(message.Chunk)
-				if err != nil {
-					 _ = res.CloseSend()
-					 break
-				}
-		 }
-		 message.Chunk = message.Chunk[:0]
+			}
+		}
+		message.Chunk = message.Chunk[:0]
 	}
 }
